44335e155b38a842e83a50a336a9997abf13c4b8
Emre Kapukaya <kapukayaemre@hotmail.com>
2024-08-01 00:02:01 +0300

    posts listed, posts test started

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Container from "@/Components/Container.vue";
+
+defineProps([
+    'posts'
+])
+</script>
+
+<template>
+    <AppLayout>
+        <Container>
+            <ul class="divide-y">
+                <li v-for="post in posts" :key="post.id" class="px-2 py-4">
+                    <span class="font-bold text-lg">{{ post.title }}</span>
+                </li>
+            </ul>
+        </Container>
+    </AppLayout>
+</template>
+
+<style scoped>
+
+</style>
