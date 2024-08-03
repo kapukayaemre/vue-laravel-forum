00d431e7b3e85964be600a9a52b461d924266c68
Emre Kapukaya <kapukayaemre@hotmail.com>
2024-08-03 19:16:41 +0300

    show single post updated

---
+++ resources/js/Pages/Posts/Show.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Container from "@/Components/Container.vue";
+import {computed} from "vue";
+import {formatDistance, parseISO} from "date-fns";
+
+const props = defineProps(['post'])
+
+const formattedDate = computed(() => formatDistance(parseISO(props.post.created_at), new Date()));
+
+</script>
+
+<template>
+    <AppLayout :title="post.title">
+        <Container>
+            <h1 class="font-bold text-2xl">{{ post.title }}</h1>
+            <span class="block mt-1 text-sm text-gray-600">{{ formattedDate }} ago by {{ post.user.name }}</span>
+
+            <article class="mt-6">
+                <pre class="whitespace-pre-wrap font-sans">{{ post.body }}</pre>
+            </article>
+        </Container>
+    </AppLayout>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Inertia\Testing\AssertableInertia;
+use function Pest\Laravel\get;
+
+it('can show a post', function () {
+    $post = Post::factory()->create();
+
+    get(route('posts.show', $post))
+        ->assertComponent('Posts/Show');
+});
+
+
+it('passes a post to the view', function () {
+    // Arrange
+    $post = Post::factory()->create();
+    $post->load('user');
+
+    // Act & Assert
+    get(route('posts.show', $post))
+        ->assertHasResource('post', PostResource::make($post));
+});
