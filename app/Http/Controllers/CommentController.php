0a3d23c5a7217360bd6b2c678f0265525ab8c50f
Emre Kapukaya <kapukayaemre@hotmail.com>
2024-08-07 22:16:34 +0300

    jetstream toasts activated

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -34,7 +34,8 @@ class CommentController extends Controller
             'user_id' => $request->user()->id,
         ]);
 
-        return to_route('posts.show', $post);
+        return to_route('posts.show', $post)
+            ->banner('Comment added successfully.');
     }
 
     /**
@@ -48,7 +49,8 @@ class CommentController extends Controller
 
         $comment->update($data);
 
-        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')]);
+        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')])
+            ->banner('Comment updated successfully.');
     }
 
     /**
@@ -60,7 +62,8 @@ class CommentController extends Controller
 
         $comment->delete();
 
-        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')]);
+        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')])
+            ->banner('Comment deleted successfully.');
 
         // return redirect()->back();
     }
